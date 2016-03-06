ff9ffad09790948cd83b96618a3fce121d5f2510
Glen Mailer <glenjamin@gmail.com>
2016-03-07 10:41:34 +1100

    Bailed out on 1.13, i'm not here to do maths!

---
+++ chapter1.13.rkt
@@ -0,0 +1,13 @@
+#lang planet neil/sicp
+
+(#%require rackunit)
+
+;; Exercise 1.13
+
+;; Got relatively far, but not all the way through the proof
+;; Showed the base inductive cases, but then
+;; tried to prove fib(n) = fib(n-1) + fib(n-2)
+;; instead of fib(n+1) = fib(n) + fib(n-1)
+;; which probably made it harder
+
+;; Here's a neat write up: http://www.kendyck.com/math/sicp/ex1-13.xml
