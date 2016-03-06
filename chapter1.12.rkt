b4c3489ad5f4b7ebc5acb25111bc66cbf62ed779
Glen Mailer <glenjamin@gmail.com>
2016-03-07 09:16:14 +1100

    1.12 without using lists

--- chapter1.12.rkt
+++ chapter1.12.rkt
@@ -16,3 +16,27 @@
       (list)
       (cons (+ (car above) (cadr above))
             (next-row (cdr above)))))
+
+(check-equal? (triangle 0) '(1))
+(check-equal? (triangle 1) '(1 1))
+(check-equal? (triangle 2) '(1 2 1))
+(check-equal? (triangle 3) '(1 3 3 1))
+(check-equal? (triangle 4) '(1 4 6 4 1))
+
+(define (triangle2 row n)
+  (cond
+    ((and (= row 0) (= n 0)) 1)
+    ((> row 0) (+ (triangle2 (dec row) (dec n))
+                  (triangle2 (dec row) n)))
+    (else 0)))
+
+(check-equal? (triangle2 0 0) 1)
+(check-equal? (triangle2 1 0) 1)
+(check-equal? (triangle2 1 1) 1)
+(check-equal? (triangle2 2 0) 1)
+(check-equal? (triangle2 2 1) 2)
+(check-equal? (triangle2 2 2) 1)
+(check-equal? (triangle2 3 0) 1)
+(check-equal? (triangle2 3 1) 3)
+(check-equal? (triangle2 3 2) 3)
+(check-equal? (triangle2 3 3) 1)
